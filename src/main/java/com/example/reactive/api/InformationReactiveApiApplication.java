47cffe50243e12308bcbba727e8a15bc81b95792
jpabloallin <jpac.647@gmail.com>
2022-06-05 22:33:56 -0500

    Initial commit

---
+++ src/main/java/com/example/reactive/api/InformationReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactive.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationReactiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationReactiveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactive/api/InformationReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactive.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationReactiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
